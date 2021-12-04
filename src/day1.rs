291d6df23c506d0183e9576990d6181d678815bd
glanham-jr <contact@grantlanham.com>
2021-12-03 23:31:07 -0500

    Day 1 Part 2

--- src/day1.rs
+++ src/day1.rs
@@ -6,24 +6,29 @@ struct SonarAccumulator {
     pub previous_value: i32,
 }
 
-pub fn p1_sonar() -> i32 {
+fn parse_line(line: Result<String, std::io::Error>) -> i32 {
+    return match line {
+        Ok(l) => match l.parse::<i32>()  {
+            Ok(num) => num,
+            Err(e) => panic!("Failed to parse as int: {:?}", e)
+        },
+        Err(e) => panic!("Failed to read line: {:?}", e)
+    }
+}
+
+fn read_file() -> BufReader<File> {
     let file_path = "data/day1_sonar.txt";
     let file =  match File::open(file_path) {
         Ok(f) => f,
         Err(e) => panic!("Failed to open file: {:?}", e),
     };
-    let reader = BufReader::new(file);
+    return BufReader::new(file);
+}
 
-    let result = reader.lines()
-        .map(|line| {
-            match line {
-                Ok(l) => match l.parse::<i32>()  {
-                    Ok(num) => num,
-                    Err(e) => panic!("Failed to parse as int: {:?}", e)
-                },
-                Err(e) => panic!("Failed to read line: {:?}", e)
-            }
-        })
+pub fn p1_sonar() -> i32 {
+    return read_file()
+        .lines()
+        .map(parse_line)
         .fold(SonarAccumulator{ count: 0, previous_value: i32::MAX }, |mut acc, n| {
             // println!("Prev: {}, Curr: {}", acc.previous_value, n);
             if n > acc.previous_value  {
@@ -31,15 +36,70 @@ pub fn p1_sonar() -> i32 {
             }
             acc.previous_value = n;
             return acc;
-        });
-
-    return result.count;
+        }).count;
 }
 
-struct SonarSlidingAccumulator {
-    // TODO
+struct SonarSlidingWindow {
+    pub lead: i32,
+    pub second: i32,
+    pub last: i32,
+}
+impl SonarSlidingWindow {
+    fn sum(&self) -> i32 {
+        return self.lead + self.second + self.last;
+    }
+    fn push(&mut self, value: i32)  {
+        self.last = self.second;
+        self.second = self.lead;
+        self.lead = value;
+    }
+}
+
+struct SonarSlidingWindowAccumulator {
+    pub lead: SonarSlidingWindow,
+    pub follow: SonarSlidingWindow,
+    pub count: i32,
 }
 
 pub fn p2_sonar() -> i32 {
-    // TODO
+    let values = read_file()
+        .lines()
+        .map(parse_line)
+        .collect::<Vec<i32>>();
+
+    if values.len() <= 4 {
+        if values.len() <= 3 {
+            return 0;
+        }
+        if values[1] + values[2] + values[3] > values[0] + values[1] + values[2] {
+            return 1
+        }
+        return 0;
+    }
+
+
+    let acc = SonarSlidingWindowAccumulator {
+        lead: SonarSlidingWindow {
+            last: values[1],
+            second: values[2],
+            lead: values[3],
+        },
+        follow: SonarSlidingWindow {
+            last: values[0],
+            second: values[1],
+            lead: values[2],
+        },
+        count: 0,
+    };
+
+    return values
+        .into_iter()
+        .fold(acc, |mut acc, n| {
+            if acc.lead.sum() > acc.follow.sum()  {
+                acc.count += 0x1;
+            }
+            acc.follow.push(acc.lead.lead);
+            acc.lead.push(n);
+            return acc;
+        }).count;
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod day1;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("day1-a: {:#?}", day1::sonar());
+    println!("day1-p1: {:#?}", day1::p1_sonar());
+    println!("day1-p2: {:#?}", day1::p2_sonar());
     Ok(())
 }
