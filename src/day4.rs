8829071395c058b756061eb1c9093171783fa14f
glanham-jr <contact@grantlanham.com>
2021-12-19 14:10:30 -0500

    Day 4 Part 2

--- src/day4.rs
+++ src/day4.rs
@@ -19,6 +19,7 @@ struct BingoGame {
     board: HashMap<u16, BingoPosition>,
     row_counts: [u16; 5],
     column_counts: [u16; 5],
+    won: bool,
 }
 impl BingoGame {
     fn new() -> Self {
@@ -26,34 +27,35 @@ impl BingoGame {
             board: HashMap::with_capacity(25),
             row_counts: [0, 0, 0, 0, 0],
             column_counts: [0, 0, 0, 0, 0],
+            won: false,
         };
     }
 }
 
-fn pretty_print(game: &BingoGame) {
-    // Not super efficient but for debugging
-    let mut string: String = String::new();
-    let mut values = game.board
-                         .values()
-                         .cloned()
-                         .into_iter()
-                         .collect::<Vec<BingoPosition>>();
-    values.sort_by(|a, b| a.column.cmp(&b.column));
-    values.sort_by(|a, b| a.row.cmp(&b.row));
+// fn pretty_print(game: &BingoGame) {
+//     // Not super efficient but for debugging
+//     let mut string: String = String::new();
+//     let mut values = game.board
+//                          .values()
+//                          .cloned()
+//                          .into_iter()
+//                          .collect::<Vec<BingoPosition>>();
+//     values.sort_by(|a, b| a.column.cmp(&b.column));
+//     values.sort_by(|a, b| a.row.cmp(&b.row));
 
-    for row in 0..5 {
-        string += r#"["#;
-        for column in 0..5 {
-            let p = values[row * 5 + column];
-            // let str_checked = if p.checked { "X" } else { " " };
-            let str_checked = if p.checked { "X" } else { " " };
-            string += &format!("({:#02}, {})", p.value, str_checked).to_string();
-        }
-        string += r#"]"#;
-        string += "\n";
-    }
-    println!("----------\n{}----------\n", string);
-}
+//     for row in 0..5 {
+//         string += r#"["#;
+//         for column in 0..5 {
+//             let p = values[row * 5 + column];
+//             // let str_checked = if p.checked { "X" } else { " " };
+//             let str_checked = if p.checked { "X" } else { " " };
+//             string += &format!("({:#02}, {})", p.value, str_checked).to_string();
+//         }
+//         string += r#"]"#;
+//         string += "\n";
+//     }
+//     println!("----------\n{}----------\n", string);
+// }
 
 struct Bingo {
     moves: Vec<u16>,
@@ -133,7 +135,6 @@ fn read_bingo() -> Bingo {
         buf.clear();
 
     }
-    println!("games {:?}", games.len());
 
     return Bingo {
         moves,
@@ -141,26 +142,33 @@ fn read_bingo() -> Bingo {
     };
 }
 
-fn _p1_bingo(bingo: &mut Bingo) -> (BingoGame, u16) {
+fn _bingo(bingo: &mut Bingo, win_break: usize) -> (BingoGame, u16) {
+    println!("win_break: {}", win_break);
+    let mut win_counter: usize = 0;
     for mv in bingo.moves.iter() {
-        println!("mv {:?}", mv);
-        for g in bingo.games.iter_mut() {
+        for g in bingo.games.iter_mut().filter(|g| !g.won) {
             match &mut g.board.borrow_mut().entry(*mv) {
                 Entry::Occupied(po) => {
                     let p = po.get_mut();
                     (*p).checked = true;
                     // po.insert(*p);
-                    // println!("mv {}, val {}, row {}, col {}", *mv, p.value, p.row, p.column);
                     g.row_counts[p.row] += 1;
                     g.column_counts[p.column] += 1;
                     if g.row_counts[p.row] == 5 {
-                        // pretty_print(g);
-                        return (Clone::clone(g), *mv);
+                        win_counter += 1;
+                        (*g).won = true;
+                        println!("wincnt: {}", win_counter);
+                        if win_counter >= win_break {
+                            return (Clone::clone(g), *mv);
+                        }
                     }
-                    if g.column_counts[p.column]  == 5 {
-                        // println!("WINNER");
-                        pretty_print(g);
-                        return (Clone::clone(g), *mv);
+                    else if g.column_counts[p.column]  == 5 {
+                        win_counter += 1;
+                        (*g).won = true;
+                        println!("wincnt: {}", win_counter);
+                        if win_counter >= win_break {
+                            return (Clone::clone(g), *mv);
+                        }
                     }
 
                 }
@@ -172,9 +180,8 @@ fn _p1_bingo(bingo: &mut Bingo) -> (BingoGame, u16) {
 }
 
 pub fn p1_bingo() -> u16 {
-    // 562 is too low
     let bingo = &mut read_bingo();
-    let (result, mv) = _p1_bingo(bingo);
+    let (result, mv) = _bingo(bingo, 1); // only need to one 1 game
     let sum = result.board
                     .values()
                     .filter(|x| !x.checked)
@@ -184,7 +191,15 @@ pub fn p1_bingo() -> u16 {
     return sum * mv;
 }
 
-pub fn p2_bingo() -> u32 {
-    let bingo = read_bingo();
-    return bingo.games.len() as u32;
+pub fn p2_bingo() -> u16 {
+
+    let bingo = &mut read_bingo();
+    let (result, mv) = _bingo(bingo, bingo.games.len()); // must win all games
+    let sum = result.board
+                    .values()
+                    .filter(|x| !x.checked)
+                    .map(|x| x.value)
+                    .sum::<u16>();
+
+    return sum * mv;
 }
